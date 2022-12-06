f79495e2f25792bcd0b51f1f3d60727dd5d1f20f
HdA <gaborneubauer.hda@gmail.com>
2022-12-06 17:16:18 +0100

    ADD SignupHandler function

--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+import { signupEndpoint } from "../App.jsx"
+
 const SignupModal = ({ onSignup, onClose }) => {
+
+    const [emailInput, setEmailInput] = useState("")
+    const [passwordInput, setPasswordInput] = useState("")
 
     return (
         <div id="login-card-blur">
@@ -11,11 +17,28 @@ const SignupModal = ({ onSignup, onClose }) => {
                         <p className="login-card-content-title-p">Registration</p>
                     </div>
                     <div id="login-card-content-inputs-container" >
-                        <input type="email" className="login-card-content-input" name="email" placeholder="email" />
-                        <input type="password" className="login-card-content-input" name="password" placeholder="password" />
+                        <input type="email"
+                            className="login-card-content-input"
+                            name="email"
+                            placeholder="email"
+                            required
+                            value={emailInput}
+                            onChange={(event) => setEmailInput(event.target.value)}
+                        />
+                        <input
+                            type="password"
+                            className="login-card-content-input"
+                            name="password"
+                            placeholder="password"
+                            required
+                            value={passwordInput}
+                            onChange={(event) => setPasswordInput(event.target.value)}
+                        />
                     </div>
                     <div id="login-card-content-button-container" >
-                        <button className="login-card-button" onClick={onSignup}>
+                        <button
+                            className="login-card-button"
+                            onClick={() => onSignup(emailInput, passwordInput, signupEndpoint)}>
                             Sign&nbsp;up
                             <span className="material-symbols-outlined">how_to_reg</span>
                         </button>
