33960bf45605106de4254b2f627e42c654b4b3a4
HdA <gaborneubauer.hda@gmail.com>
2022-12-06 22:48:56 +0100

    ADD User's area html skeleton with React

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,13 @@ function App() {
   const [isLogin, setIsLogin] = useState(false)
   // User logged in state
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-
+  // User area upload section toggle and inputs
+  const [uploadToggle, setUploadToggle] = useState(false)
+  const [uploadTitleInput, setUploadTitleInput] = useState("")
+  const [uploadDescriptionInput, setUploadDescriptionInput] = useState("")
+  const [uploadFileInputValue, setUploadFileInputValue] = useState("")
+  const [uploadSelectedFile, setUploadSelectedFile] = useState({})
+  const [uploadServerMessage, setUploadServerMessage] = useState("Upload response here")
 
   // ! START: On Window Load - set App state
   const loadData = async () => {
@@ -77,8 +83,6 @@ function App() {
   }
 
 
-
-
   // ! Backend IP: backendApi
   // Signup handler
   const signupHandler = async (email, password, endpoint) => {
@@ -100,6 +104,27 @@ function App() {
     }
     if (responseStatus === 401) { alert("Wrong e-mail or password.") }
   }
+
+
+
+
+
+
+
+
+  // ! Search handler for user's database (title, desc?)
+  // ? Ezt majd implementálni kell a museum api search handlerből.
+  // *Lilla? :)
+
+  // Upload Handler - Hozom: Gábor
+
+  // Delete Handler - Hozom: Gábor
+
+  // Edit handler - Ez még kicsit homály, talán modalban kellene...
+
+
+
+
 
 
 
@@ -147,9 +172,105 @@ function App() {
 
             {isLoggedIn &&
               <div>
-                <h1>
-                  Hey, this is user page
-                </h1>
+                <div id="user-section-container">
+
+                  <div id="user-header-section">
+                    <div id="user-header-greeting-container">
+                      <p id="user-greeting">Your Gallery</p>
+                    </div>
+                    <div id="user-header-button-container">
+                      <button
+                        id="user-header-upload-button"
+                        onClick={() => {
+                          setUploadToggle(!uploadToggle)
+                        }}>
+                        {(!uploadToggle) ? "Open upload panel" : "Close upload panel"}
+                        <span className="material-symbols-outlined">upload</span>
+                      </button>
+                    </div>
+                    <div id="user-header-search-container">
+                      <div className="tooltip">
+                        <input
+                          className="search-input"
+                          type="search"
+                          name="search-users"
+                          id="search-users"
+                          placeholder="search my gallery"
+                          spellCheck="false"
+                        // onKeyDown={(event) => {
+                        //   if (event.key === "Enter") {
+                        //     onSearch(event.target.value)
+                        //     // document.getElementById("image-grid").scrollIntoView();
+                        //   }
+                        // }}
+                        />
+                        <span className="tooltiptext">
+                          Search among your saved pictures:
+                          Type keyword than press Enter.
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  {uploadToggle &&
+                    <div id="user-upload-section">
+
+                      <div id="user-upload-input-container">
+                        <input
+                          type="text"
+                          className="user-upload-input"
+                          placeholder="title"
+                          maxLength="40"
+                          value={uploadTitleInput}
+                          onChange={event => setUploadTitleInput(event.target.value)}
+                        />
+
+                        <input
+                          type="text"
+                          className="user-upload-input"
+                          placeholder="description"
+                          maxLength="40"
+                          value={uploadDescriptionInput}
+                          onChange={event => setUploadDescriptionInput(event.target.value)}
+                        />
+
+                        <input
+                          type="file"
+                          id="user-upload-file-input"
+                          className="user-upload-input"
+                          value={uploadFileInputValue}
+                          onChange={(event) => {
+                            setUploadSelectedFile(event.target.files[0])
+                            setUploadFileInputValue(event.target.value)
+                          }}
+                        />
+
+                        <div id="user-upload-message-container">
+                          <p id="user-upload-message">
+                            {uploadServerMessage}
+                          </p>
+                        </div>
+                      </div>
+
+                      <div id="user-upload-button-container">
+                        <input
+                          type="submit"
+                          id="user-upload-button"
+                          value="Upload!"
+                        // onClick={uploadHandler}
+                        />
+                      </div>
+                    </div>
+                  }
+
+                  <div id="user-image-grid-section">
+
+                    // ! image grid component ???
+
+                  </div>
+                  
+                </div>
+
               </div>
             }
 
@@ -175,9 +296,9 @@ function App() {
             />}
 
         </div>
-      </main>
+      </main >
 
-    </div>
+    </div >
   )
 }
 
